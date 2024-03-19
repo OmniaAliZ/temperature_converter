f5ef1cab5082ae4959942448ee5d820b5d6e98b2
OmniaAliZ <zzomniaa@gmail.com>
2024-03-19 22:40:57 +0300

    DESIGN

--- src/Program.cs
+++ src/Program.cs
@@ -1,37 +1,53 @@
-﻿// Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine();
+System.Console.WriteLine();
+System.Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("╔═════════════════════════════════╗");
+Console.WriteLine("║                                 ║");
+Console.WriteLine("║           WELCOME TO            ║");
+Console.WriteLine("║      TEPERATURE CONVERTOR       ║");
+Console.WriteLine("║                                 ║");
+Console.WriteLine("╚═════════════════════════════════╝");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine();
-Console.WriteLine("------- Level 1 -------");
+Console.WriteLine("------------  Level 1  ------------");
 Console.WriteLine();
 
+Console.ForegroundColor = ConsoleColor.Blue;
 TempConvert tempF = new(32, 'f');
 TempConvert tempC = new(100, 'c');
-Console.WriteLine(tempF.Converted());
-Console.WriteLine(tempC.Converted());
-
 Console.WriteLine();
-Console.WriteLine("------- Level 2 -------");
+Console.WriteLine("32 F Converted :" + tempF.Converted());
+Console.WriteLine();
+Console.WriteLine("100 C Converted :" + tempC.Converted());
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine();
+Console.WriteLine("------------  Level 2  ------------");
 Console.WriteLine();
 
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
 Console.WriteLine("Enter a temperature and its unit (C or F):");
+Console.ForegroundColor = ConsoleColor.White;
 string? inputL2 = Console.ReadLine();
 string[] infoL2 = inputL2.Split(' ');
 double temperatureL2 = double.Parse(infoL2[0]);
-char unitL2 = Convert.ToChar(infoL2[1]);
+char unitL2 = Convert.ToChar(infoL2[1].ToLower());
 TempConvert measureL2 = new(temperatureL2, unitL2);
-Console.WriteLine("Converted:" + measureL2.Converted());
-
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine();
-Console.WriteLine("------- Level 3 -------");
+Console.WriteLine($"{temperatureL2} {unitL2} Converted = " + measureL2.Converted());
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine();
+Console.WriteLine("------------  Level 3  ------------");
 Console.WriteLine();
 
-//     Console.WriteLine("Enter a temperature :");
-//     double temp = Convert.ToDouble(Console.ReadLine());
-//     Console.WriteLine("Enter its unit :");
-//     char unit = Convert.ToChar(Console.ReadLine());
-//     TempConvert measure = new(temp, unit);
-
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
 Console.WriteLine("Enter a temperature and its unit (C or F), or type 'Quit' to exit:");
-
+Console.ForegroundColor = ConsoleColor.White;
 string? input = Console.ReadLine();
 Console.WriteLine();
 while (true)
@@ -40,12 +56,13 @@ while (true)
     {
         if (input is null || input == "" || input.Equals("quit", StringComparison.CurrentCultureIgnoreCase))
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("PROGRAM STOPPED!");
             Console.WriteLine();
             break;
         }
         string[] info = input.Split(' ');
-        string unit = Convert.ToString(info[1]);
+        string unit = Convert.ToString(info[1].ToLower());
         if (double.TryParse(info[0], out double r))
         {
             double temperature = double.Parse(info[0]);
@@ -62,38 +79,37 @@ while (true)
         {
             throw new NonNumericException("non-numeric values");
         }
-
     }
     catch (NonNumericException ex)
     {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Error occurred: " + ex.Message);
-        Console.WriteLine("PROGRAM STOPPED!");
+        Console.WriteLine("TEPERATURE IS NUMBER ONLY!");
         Console.WriteLine();
-        break;
     }
     catch (UnsupportedException ex)
     {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Error occurred: " + ex.Message);
-        Console.WriteLine("PROGRAM STOPPED!");
+        Console.WriteLine("ONLY CELSIUS OR FAHRENHIET!");
         Console.WriteLine();
-        break;
     }
     catch (Exception ex)
     {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Error occurred: " + ex.Message);
-        Console.WriteLine("PROGRAM STOPPED!");
+        Console.WriteLine("PLEASE ENTER VALID VALUES!");
         Console.WriteLine();
-        break;
     }
-
-    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
     Console.WriteLine("Enter a temperature and its unit (C or F), or type 'Quit' to exit:");
+    Console.ForegroundColor = ConsoleColor.White;
     input = Console.ReadLine();
     Console.WriteLine();
     continue;
 }
 
-Console.WriteLine("--------------");
+Console.WriteLine("------------  ------------  ------------");
 
 class TempConvert
 {
@@ -126,25 +142,26 @@ class TempConvert
         double? converted;
         if (measurementUnit is "f")
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             converted = Math.Round((temperatureAmount - 32) / (1.8), 2);
             Console.WriteLine($"{temperatureAmount} f Converted is : {converted} c");
             Console.WriteLine();
         }
         else if (measurementUnit is "c")
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             converted = Math.Round(((1.8) * temperatureAmount) + 32, 2);
             Console.WriteLine($"{temperatureAmount} c Converted is : {converted} f");
             Console.WriteLine();
-
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("not fahrenheit nor celsius");
             Console.WriteLine();
         }
     }
 }
-
 public class UnsupportedException : Exception
 {
 
